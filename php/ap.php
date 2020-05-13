3ec8334c101eb5fb2c8e370cb5120146b7de7538
Allex Nogue <38998091+AllexNogue@users.noreply.github.com>
2020-05-13 16:01:32 -0300

    Create ap.php

---
+++ php/ap.php
@@ -0,0 +1,20 @@
+<?php
+
+$array = ["Amapá",
+"Calçoene",
+"Cutias",
+"Ferreira Gomes",
+"Itaubal",
+"Laranjal do Jari",
+"Macapá",
+"Mazagão",
+"Oiapoque",
+"Pedra Branca do Amapari",
+"Porto Grande",
+"Pracuuba",
+"Santana",
+"Serra do Navio",
+"Tartarugalzinho",
+"Vitória do Jari"];
+
+?>
