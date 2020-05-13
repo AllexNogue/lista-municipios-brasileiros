5f33295b090d9a526faa4cac11f400c1b88c227c
Allex Nogue <38998091+AllexNogue@users.noreply.github.com>
2020-05-13 16:02:30 -0300

    Create ac.php

---
+++ php/ac.php
@@ -0,0 +1,28 @@
+<?php
+
+$array = [
+    "Acrelândia",
+    "Assis Brasil",
+    "Brasileia",
+    "Bujari",
+    "Capixaba",
+    "Cruzeiro do Sul",
+    "Epitaciolândia",
+    "Feijó",
+    "Jordão",
+    "Mâncio Lima",
+    "Manoel Urbano",
+    "Marechal Thaumaturgo",
+    "Xapuri",
+    "Plácido de Castro",
+    "Porto Acre",
+    "Porto Walter",
+    "Rio Branco",
+    "Rodrigues Alves",
+    "Santa Rosa do Purus",
+    "Senador Guiomard",
+    "Sena Madureira",
+    "Tarauacá",
+];
+
+?>
